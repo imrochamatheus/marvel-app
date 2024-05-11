09214d65237c095d2c9edc253b5720f6b3242660
Matheus Rocha <im.rochamatheus@gmail.com>
2024-05-11 19:26:48 -0300

    :sparkles: feat: adicionar serviço para obtenção de dados dos cartões

--- services/media.ts
+++ services/media.ts
@@ -1,15 +1,11 @@
 import { MediaType, PreviewCardProps } from "@/types/previewCard.types";
 
-export async function fetchMedia(
-  type: MediaType
-): Promise<PreviewCardProps[] | null> {
+export async function fetchMedia(type: MediaType): Promise<any> {
   try {
     const response = await fetch(`/api/media?type=${type}`);
-
     if (!response.ok) {
       throw new Error("Erro ao buscar os dados dos personagens");
     }
-
     const characters: PreviewCardProps[] = await response.json();
     return characters;
   } catch (error) {
