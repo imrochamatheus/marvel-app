73779af241540aaf329acac1e07454a298ee7b55
Matheus Rocha <im.rochamatheus@gmail.com>
2024-05-11 13:11:19 -0300

    :sparkles: feat: adicionar serviço para obter informações de mídia

---
+++ services/media.ts
@@ -0,0 +1,19 @@
+import { MediaType, PreviewCardProps } from "@/types/previewCard.types";
+
+export async function fetchMedia(
+  type: MediaType
+): Promise<PreviewCardProps[] | null> {
+  try {
+    const response = await fetch(`/api/media?type=${type}`);
+
+    if (!response.ok) {
+      throw new Error("Erro ao buscar os dados dos personagens");
+    }
+
+    const characters: PreviewCardProps[] = await response.json();
+    return characters;
+  } catch (error) {
+    console.error("Erro ao buscar os dados dos personagens:", error);
+    return null;
+  }
+}
