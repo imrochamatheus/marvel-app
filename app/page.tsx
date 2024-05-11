a313c020406eb3ec1ebf44609c164ec93df8572f
Matheus Rocha <im.rochamatheus@gmail.com>
2024-05-10 23:30:19 -0300

    :recycle: refactor: remover previewCard em app/page

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import PreviewCard from "./ui/PreviewCard";
 
 export default function Home() {
   return (
-    <>
-      <PreviewCard />
-    </>
+    <div>
+      <h1>App</h1>
+    </div>
   );
 }
