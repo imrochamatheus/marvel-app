e41862ccda7aa6ccc2f0b12376063b185ea436dc
Matheus Rocha <im.rochamatheus@gmail.com>
2024-05-11 13:08:01 -0300

    :sparkles: feat: adicionar setup para styled-components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 
-import StyledComponentsRegistry from "./lib/registry";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import StyledComponentsRegistry from "../lib/registry";
 
 const inter = Inter({ subsets: ["latin"] });
 
